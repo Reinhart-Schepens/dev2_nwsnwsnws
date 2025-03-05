71728474e16cb605318ee79a6e7caed669263601
Reinhart-Schepens <reinhart.schepens@student.arteveldehs.be>
2025-03-05 23:53:32 +0100

    1

--- server/routes.ts
+++ server/routes.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import path from "path";
+import { getNews,getNewsBySlug } from "./newsService";
 
 const router = express.Router();
 
@@ -7,8 +8,21 @@ const router = express.Router();
  * GET / - Laadt de homepagina
  */
 router.get("/", (req: Request, res: Response): void => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+    const items = getNews();
+    res.render("index", { 
+        title: "News",
+        items: items
+    });
 });
 
+router.get("/details/:slug",  (req: Request, res: Response): void => {
+    const slug = req.params.slug;
+    const news = getNewsBySlug(slug);
+    // if news === undefined : redirect to 404
+    res.render("details", {
+        title: "News details",
+        news: news
+    });
+});
 
 export default router;
