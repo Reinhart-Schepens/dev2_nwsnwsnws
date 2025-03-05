89e27afa38e47de57b43f3fa86c1dc3e07810d67
Reinhart-Schepens <reinhart.schepens@student.arteveldehs.be>
2025-03-05 22:37:47 +0100

    1

---
+++ server/routes.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+import path from "path";
+
+const router = express.Router();
+
+/**
+ * GET / - Laadt de homepagina
+ */
+router.get("/", (req: Request, res: Response): void => {
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+
+export default router;
